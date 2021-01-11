4d080e93740ed71da617b7e1b43418f0b6ffa5a3
Traijan <weberalexander00@web.de>
2021-01-11 15:10:49 +0100

    Initial Commit

---
+++ app/src/main/java/de/traijan/farbcode/MainActivity.java
@@ -0,0 +1,13 @@
+package de.traijan.farbcode;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
